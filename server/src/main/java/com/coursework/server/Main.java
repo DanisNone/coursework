298247e8cf46843b6b2de235df8090fe31313287
DanisNone <gubajdullindanis0@gmail.com>
2025-12-21 20:35:47 +0500

    remove ServerJavalin.java to Server.java

--- server/src/main/java/com/coursework/server/Main.java
+++ server/src/main/java/com/coursework/server/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static public void main(String[] args) throws IOException {
         String hostname = "10.25.248.179";
         int port = 8080;
-        ServerJavalin server = new ServerJavalin(hostname, port);
+        Server server = new Server(hostname, port);
         server.start();
     }
 }
