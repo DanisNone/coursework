c3d44f9105f40be6d17a3a86c6137662fa1f6a90
DanisNone <gubajdullindanis0@gmail.com>
2025-12-21 20:38:14 +0500

    correct hostname

--- server/src/main/java/com/coursework/server/Main.java
+++ server/src/main/java/com/coursework/server/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     static public void main(String[] args) throws IOException {
-        String hostname = "10.25.248.179";
+        String hostname = "193.108.113.136";
         int port = 8080;
         Server server = new Server(hostname, port);
         server.start();
